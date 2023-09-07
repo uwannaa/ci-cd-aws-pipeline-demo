494ae0069ff3dca8a48d2ee68c08e76391193049
uwannaa <yoanna.krasteva@severalclouds.com>
2023-09-07 13:44:12 +0300

    Initial commit

---
+++ lib/ci-cd-aws-pipeline-demo-stack.ts
@@ -0,0 +1,23 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
+import { ManualApprovalStep } from 'aws-cdk-lib/pipelines';
+// import { MyPipelineAppStage } from './stage';
+
+export class CiCdAwsPipelineDemoStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+      new CodePipeline(this, 'Pipeline', { 
+      pipelineName: 'TestPipeline',
+      synth: new ShellStep('Synth', {
+        input: CodePipelineSource.gitHub('uwannaa/ci-cd-aws-pipeline-demo','main'),
+        commands: [
+          'npm ci',
+          'npm run build',
+          'npx cdk synth'
+        ]}),
+      });
+
+  }
+}
